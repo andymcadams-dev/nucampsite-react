fee18d2ae80b75e187c36531bacd3aea1dbcc1b3
Andy McAdams <andymcadams-dev@gmail.com>
2021-09-04 07:31:58 -0500

    Adding to the Single Page App

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -4,9 +4,9 @@ import { Card,
         CardImgOverlay,  
         CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
     return (
-                    <Card onClick={() => onClick(campsite.id)}>
+                    <Card>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -20,7 +20,7 @@ function Directory(props){
         const directory = props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                    <RenderDirectoryItem campsite={campsite} />
                 </div>
             )
         });
