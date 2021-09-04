706b2ddaa775ac80b6886f9db62363d76bb59dc9
Andy McAdams <andymcadams-dev@gmail.com>
2021-09-04 06:27:54 -0500

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -24,6 +24,7 @@ class Main extends Component {
               <Header />
               <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
               <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/> 
+              <Footer />
           </div>
       );
   }
