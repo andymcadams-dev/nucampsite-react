b95b26a6c170bda59ccd6d0fd2bbc49525b91567
Andy McAdams <andymcadams-dev@gmail.com>
2021-09-04 07:44:10 -0500

    React Router Paramters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -3,14 +3,18 @@ import { Card,
         CardImg, 
         CardImgOverlay,  
         CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 function RenderDirectoryItem({campsite}) {
     return (
                     <Card>
+                        <Link to={`/directory/${campsite.id}`}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
                         </CardImgOverlay>
+                        </Link>
                     </Card>
     )
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -35,6 +35,18 @@ class Main extends Component {
           />
       );
     }
+
+    const CampsiteWithId = ({match}) => {
+      return(
+        <CampsiteInfo 
+          campsite={this.state.campsites.filter(campsite => campsite.id === 
+          +match.params.campsiteId)[0]}
+          comments={this.state.comments.filter(comment => comment.campsiteId === 
+          +match.params.campsiteId)} 
+          />
+      );
+    }
+
       return (
           <div> 
               <Header />
@@ -42,6 +54,7 @@ class Main extends Component {
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/directory' render={() => <Directory
                  campsites={this.state.campsites} />} />
+                 <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                 <Route exact path='/contactus' component={Contact} />
                 <Redirect to='/home' />
               </Switch>
